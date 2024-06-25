64050032d95a195586c3899a9ba9972d17f28879
MichaelHoha <michael1hoha@gmail.com>
2024-06-25 18:01:22 +0300

    Adding typescript to project and modified .gitignore a little

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import HomePage from './components/home/homePage';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomePage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/homePage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import profile from '../../resources/Profile.jpg'
+
+
+
+
+function HomePage() {
+    return (
+        <div>
+            <h1>
+                Welcom to Michael's Portofolio
+            </h1>
+
+            <img src={profile} className="App-logo" alt="logo" />
+        </div>
+    )
+}
+
+
+export default HomePage;    
