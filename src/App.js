8dc2b0997e4bfcb990201029541779bffb7853c5
michaelhoha <michael1hoha@gmail.com>
2024-06-25 11:16:09 +0300

    Adding a new repository for my Portofolio project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import profile from './resources/Profile.jpg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+            Welcom to Michael's Portofolio
+        </h1>
+        
+        <img src={profile} className="App-logo" alt="logo" />
       </header>
     </div>
   );
