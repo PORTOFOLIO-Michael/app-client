29d5b47120010fcecac359773a388b17d5d24a6f
michaelhoha <michael1hoha@gmail.com>
2024-07-11 09:58:06 +0300

    Minor

--- src/components/home/homePageBody.tsx
+++ src/components/home/homePageBody.tsx
@@ -7,14 +7,16 @@ export default function HomePageBody() {
     return (
         <div className="body_container">
             <span className="timeline_title">My Timeline</span>
-            <div className="time_line">Front-end developer course at SVCollege , Tel - Aviv: 2019 - 2020</div>
-            <div className="time_line">
-                B.Sc. in Computer science at Afeka Academic College of Engineering: 2021 - 2024
-            </div>
 
             <div className="time_line">
                 QA & Customer support - Student position at Moveo Group, Tel Aviv: 2022 - 2024
             </div>
+
+            <div className="time_line">
+                B.Sc. in Computer science at Afeka Academic College of Engineering: 2021 - 2024
+            </div>
+
+            <div className="time_line">Front-end developer course at SVCollege , Tel - Aviv: 2019 - 2020</div>
 
             <div className="time_line">
                 MILITARY SERVE - Three years of service in a special unit in the Givati Brigade: 2013 - 2016
