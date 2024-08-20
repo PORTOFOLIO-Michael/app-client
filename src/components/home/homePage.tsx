0efa2def9678d9c7a3a676b025b7840b46b60c58
michaelhoha <michael1hoha@gmail.com>
2024-08-20 14:27:36 +0300

    Updated logo, and added projects component

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -3,14 +3,17 @@ import Header from "./header";
 import Body from "./homePageBody";
 
 import './homePage.css'
+import Projects from "components/projects/projects";
 
 function HomePage() {
-    
+
     return (
         <div className="home_page">
             <Header />
-            
+
             <Body />
+
+            <Projects />
 
         </div>
     )
---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+export default function Projects() {
+
+    interface Project {
+        name: string;
+        website: string;
+    }
+
+    const [projects, setProjects] = useState<Project | null>(null);
+
+    useEffect(() => {
+        setProjects({
+            name: 'Clip Cloud',
+            website: 'http://34.255.196.211:3000/'
+        });
+    }, []);
+
+    return (
+
+        <div>
+            <h1>Projects</h1>
+
+        </div>
+    )
+}
+
+
