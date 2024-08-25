92b99065f3067c9f287b425eb200e189968e4a72
michaelhoha <michael1hoha@gmail.com>
2024-08-25 21:51:11 +0300

    Added some new styles

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,29 +1,46 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 
+interface Project {
+    name: string;
+    github: string;
+    linkToProject: string,
+    technologies: {
+        front: string,
+        back: string,
+        additional: string,
+    }
+}
 
 export default function Projects() {
-
-    interface Project {
-        name: string;
-        website: string;
-    }
 
     const [projects, setProjects] = useState<Project | null>(null);
 
     useEffect(() => {
-        setProjects({
-            name: 'Clip Cloud',
-            website: 'http://34.255.196.211:3000/'
-        });
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('./info.json');
+                setProjects(response.data);
+                console.log("Fetching data...");
+            } catch (error) {
+                console.log("Erro on fetching data");
+            }
+        }
+
+        fetchData();
     }, []);
 
     return (
         <div className='projects_container'>
 
             <h1 className='projects_title'>Projects</h1>
-            <span>{projects?.name}</span>
-            <span>{projects?.website}</span>
+            {/* <span>{projects?.name}</span>
+            <span>{projects?.github}</span>
+            <span>{projects?.linkToProject}</span>
+            <span>{projects?.technologies.front}</span>
+            <span>{projects?.technologies.back}</span>
+            <span>{projects?.technologies.additional}</span> */}
 
         </div>
     )
